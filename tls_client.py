f13a51da242885e81c9a3676d69ce8520e1da8df
Sang Cá Mập <55171560+sangcamap@users.noreply.github.com>
2021-11-30 22:26:59 +0700

    Update and rename client.py to tls_client.py

--- tls_client.py
+++ tls_client.py
@@ -3,15 +3,15 @@ from ssl import SSLContext, PROTOCOL_TLS_CLIENT
 
 
 hostname='example.org'
-ip = '127.0.0.1'
-port = 8443
+ip = '192.168.191.133'
+port = 3000
 context = SSLContext(PROTOCOL_TLS_CLIENT)
 context.load_verify_locations('cert.pem')
 
 with create_connection((ip, port)) as client:
     with context.wrap_socket(client, server_hostname=hostname) as tls:
         print(f'Using {tls.version()}\n')
-        tls.sendall(b'Hello, world')
+        tls.sendall(b'Hello Nguyen Quang Sang')
 
         data = tls.recv(1024)
         print(f'Server says: {data}')
